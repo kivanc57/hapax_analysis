3c017db2caf68974a2e6ae5a5d6424b38112eaa6
Kivanc Gordu <kivancgordu@hotmail.com>
2025-04-25 14:52:41 +0200

    Start write.go Rename read_utils.go -> read.go Rename write_utils.go -> write.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module hapax_analysis
+
+go 1.23.8
+
+require (
+	github.com/aaaton/golem/v4 v4.0.1 // indirect
+	github.com/aaaton/golem/v4/dicts/en v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	utils"hapax_analysis/utils"
+)
+
+func main() {
+	absDirPath := "/home/kivanc57/Desktop/hapax_analysis/data"
+	var sb strings.Builder
+
+  dir  := utils.ReadDir(absDirPath)
+	for _, f := range dir {
+		file_name := f.Name()
+
+		if strings.HasSuffix(file_name, ".txt") {
+			absFilePath := filepath.Join(absDirPath, file_name)
+			sb.WriteString(utils.ReadWriteText(absFilePath))
+		}
+	}
+	corpus := sb.String()
+	freqMap := utils.GetFreqMap(corpus, true)
+	for word, count := range freqMap {
+    fmt.Printf("%-15s → %d\n", word, count)
+  }
+}
+
